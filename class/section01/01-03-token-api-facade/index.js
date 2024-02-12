f7dc457e591cd7f7962e0e71c47a412101438e82
sodamkimkim <theka265@gmail.com>
2024-02-12 16:47:37 +0900

    01-03-token-api-facade

---
+++ class/section01/01-03-token-api-facade/index.js
@@ -0,0 +1,22 @@
+function checkPhoneNum(str) {
+    if (str.length < 10 || str.length > 11) { // early-exit pattern : 일찍 에러를 찾아서 함수 종료 시켜버린다.
+        console.log("휴대폰 자릿수 불합격");
+        return false;
+    } else return true;
+}
+function getToken() {
+    const resultToken = String(Math.floor(Math.random() * 1000000)).padStart(6, "0");
+    console.log(resultToken);
+}
+function sendTokenToSMS(str, resultToken) {
+    console.log(`${str}번호로 인증번호 ${resultToken}을 전송합니다.`);
+}
+
+function createTokenOfPhone(str) {
+    const isValid = checkPhoneNum(str);
+    if (isValid === false) return;
+
+    sendTokenToSMS(str, getToken());
+}
+
+createTokenOfPhone("01012345678");
